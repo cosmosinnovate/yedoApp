4c8787c0d2c35aaa764a67aa109880ca95fec2e1
Taban Cosmos <cosmostaban@gmail.com>
2024-08-31 15:57:10 -0700

    Increase version

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Text } from "react-native";
+import { Platform, Text } from "react-native";
 
-function AppText({ children, mH=0, mV=0, color, weight="400", size=16, textAlign='center', textDecoration="none", ...other }) {
+function AppText({ children, mH = 0, mV = 0, color, weight = "400", size = 16, textAlign = 'center', textDecoration = "none", ...other }) {
   return (
     <Text
       style={[{ marginHorizontal: mH, marginVertical: mV }, {
         fontSize: size,
         textAlign: textAlign,
         fontWeight: weight,
-        fontFamily: Platform.OS==="android"? "Roboto":"Avenir",
+        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
       }, { color: color, textDecorationLine: textDecoration }]}
       {...other}>
       {children}
